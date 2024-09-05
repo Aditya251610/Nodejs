f192039184b7304cf951a9124b25950c378b0c92
Aditya251610 <aditya.sharma16062016@gmail.com>
2024-09-05 08:03:17 +0530

    learned sending static files

---
+++ assingment3/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path')
+const homeRouter = require('./routes/home');
+const usersRouter = require('./routes/users');
+
+app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(homeRouter);
+app.use(usersRouter);
+
+app.listen(3000);
